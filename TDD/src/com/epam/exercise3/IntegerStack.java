11a6724ad1d9611dabbeb8b98e33d1b81f60f31d
Peter Horvath <Peter_Horvath@epam.com>
2015-02-18 13:49:53 +0100

    refactor IntegerStackTest

--- TDD/src/com/epam/exercise3/IntegerStack.java
+++ TDD/src/com/epam/exercise3/IntegerStack.java
@@ -3,46 +3,31 @@ package com.epam.exercise3;
 public class IntegerStack {
 	private Integer[] stack = new Integer[0];
 
-	public void push(Integer number) {
-		validateNotNull(number);
-		final int stackLength = stack.length;
-		final int newStackLength = stackLength + 1;
-		final int newValuePos =  newStackLength - 1;
-
-		final Integer[] newStack = new Integer[newStackLength];
-
-		for (int i = 0; i < stackLength; i++) {
-			newStack[i] = stack[i];
-		}
-		newStack[newValuePos] = number;
-		stack = newStack;
-	}
-
-	private void validateNotNull(Integer number) {
-		if (number == null) {
-			throw new IllegalArgumentException();
-		}
-	}
-
 	public Integer pop() {
 		checkStackIfEmpty();
 		final int lastItemPos = stack.length - 1;
 		final int newStackLength = stack.length - 1;
-		
+
 		Integer result = stack[lastItemPos];
-		final Integer[] newStack = new Integer[newStackLength];
-		
-		for (int i = 0; i < newStackLength; i++) {
-			newStack[i] = stack[i];
-		}
+
+		final Integer[] newStack = copyStackToNewStack(newStackLength,
+				newStackLength);
+
 		stack = newStack;
 		return result;
 	}
 
-	private void checkStackIfEmpty() {
-		if (isEmpty()) {
-			throw new IllegalStateException();
-		}
+	public void push(Integer number) {
+		validateNotNull(number);
+		final int stackLength = stack.length;
+		final int newStackLength = stackLength + 1;
+		final int newValuePos = newStackLength - 1;
+
+		final Integer[] newStack = copyStackToNewStack(stackLength,
+				newStackLength);
+
+		newStack[newValuePos] = number;
+		stack = newStack;
 	}
 
 	public boolean isEmpty() {
@@ -51,5 +36,27 @@ public class IntegerStack {
 
 	public int count() {
 		return stack.length;
+	}
+	
+	private Integer[] copyStackToNewStack(final int stackLength,
+			final int newStackLength) {
+		final Integer[] newStack = new Integer[newStackLength];
+
+		for (int i = 0; i < stackLength; i++) {
+			newStack[i] = stack[i];
+		}
+		return newStack;
+	}
+
+	private void validateNotNull(Integer number) {
+		if (number == null) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void checkStackIfEmpty() {
+		if (isEmpty()) {
+			throw new IllegalStateException();
+		}
 	}
 }
