c6f6166ce813250b4abb611bbef36612d013f5af
Peter Horvath <Peter_Horvath@epam.com>
2015-02-24 17:21:16 +0100

    andris

--- Torpedo/src/com/epam/torpedo/field/BattleField.java
+++ Torpedo/src/com/epam/torpedo/field/BattleField.java
@@ -3,6 +3,6 @@ package com.epam.torpedo.field;
 
 public interface BattleField {
 	public boolean isFound(FieldCoordinate point);
-	public void foundShip(FieldCoordinate nextPoint);
+	public void foundShip(FieldCoordinate point);
 	public void fillRandomShips();
 }
--- Torpedo/test/com/epam/torpedo/field/DefaultBattleFieldTest.java
+++ Torpedo/test/com/epam/torpedo/field/DefaultBattleFieldTest.java
@@ -1,6 +1,11 @@
 package com.epam.torpedo.field;
 
+import org.testng.annotations.Test;
+
 
 public class DefaultBattleFieldTest {
-	
+	@Test
+	public void testFillRandomShipsShouldThrowErrorWhenTooManyShips(){
+		
+	}
 }
